ddd25567fe54b5b74c9032a04d50f87d5d560ca7
Anshul Dutta <AnshulKumar.Dutta@news.co.uk>
2017-02-16 14:21:49 +0000

    First excerise attempted. Should have passed on that as it mathes the requird output

--- machine-learning-ex5/ex5/ex5.m
+++ machine-learning-ex5/ex5/ex5.m
@@ -35,12 +35,12 @@ load ('ex5data1.mat');
 m = size(X, 1);
 
 % Plot training data
-plot(X, y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
-xlabel('Change in water level (x)');
-ylabel('Water flowing out of the dam (y)');
+% plot(X, y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
+% xlabel('Change in water level (x)');
+% ylabel('Water flowing out of the dam (y)');
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+% fprintf('Program paused. Press enter to continue.\n');
+% pause;
 
 %% =========== Part 2: Regularized Linear Regression Cost =============
 %  You should now implement the cost function for regularized linear 
@@ -53,6 +53,7 @@ J = linearRegCostFunction([ones(m, 1) X], y, theta, 1);
 fprintf(['Cost at theta = [1 ; 1]: %f '...
          '\n(this value should be about 303.993192)\n'], J);
 
+quit
 fprintf('Program paused. Press enter to continue.\n');
 pause;
 
